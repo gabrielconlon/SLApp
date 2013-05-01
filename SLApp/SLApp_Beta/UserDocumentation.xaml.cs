14701f150b16b65206ef2c9bbb1530b9c638893e
Gabriel Conlon <gconlon13@my.whitworth.21>
2013-05-01 14:25:15 -0700

    redo of last commit

---
+++ SLApp/SLApp_Beta/UserDocumentation.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Documents;
+
+namespace SLApp_Beta
+{
+	/// <summary>
+	/// Interaction logic for UserDocumentation.xaml
+	/// </summary>
+	public partial class UserDocumentation : Window
+	{
+		public UserDocumentation()
+		{
+			InitializeComponent();
+		}
+	}
+}
